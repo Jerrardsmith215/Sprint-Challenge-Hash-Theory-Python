7ec1e419398040640bbeec3f23e52c5f67f7b47d
unknown <jerrardsmith215@gmail.com>
2019-08-03 16:30:38 -0400

    ex1 attempted

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -9,15 +9,36 @@ from hashtables import (HashTable,
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+    if length == 1:
+        result = None
 
-    return None
+    # The weight of the item is its key, which decides its index on the table
+    # The location of the item is its place in its original array.
 
+    # add items to hashtable
+    for location, weight in enumerate(weights):
+        hash_table_insert(ht, weight, location)
+    
+    # TODO
+    # return their indexes
+    
+    # search through values of linked pairs
+    for location, weight in enumerate(weights):
+        # starting point
+        start = weight
+        # remainder
+        remainder = limit - start
+        search = hash_table_retrieve(ht, remainder) # search for remainder
+        if search is not None:
+            if search > location:
+                result = (search, location)
+            elif search < location:
+                result = (location, search)
+
+    return print_answer(result)
 
 def print_answer(answer):
     if answer is not None:
-        print(str(answer[0] + " " + answer[1]))
+        print(f'{answer[0]} {answer[1]}')
     else:
         print("None")
